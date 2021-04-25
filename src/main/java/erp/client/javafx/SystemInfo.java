5db9bd0c7384de6d41fc23878323907a1a2aa0ab
Shahul <shahul1433@gmail.com>
2021-04-25 23:48:50 +0530

    initial commit

---
+++ src/main/java/erp/client/javafx/SystemInfo.java
@@ -0,0 +1,13 @@
+package erp.client.javafx;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
